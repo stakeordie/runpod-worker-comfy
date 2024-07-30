d443ef693cd9b8e27a5e7581e14b30cdb6d657b2
Luis Espinoza <79593471+luiseel@users.noreply.github.com>
2024-07-29 22:03:12 -0700

    Add symbolic links to custom_nodes dir

--- src/start.sh
+++ src/start.sh
@@ -4,6 +4,8 @@
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
+ln -s /runpod-volume/custom_nodes /comfyui/custom_nodes
+
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
     echo "runpod-worker-comfy: Starting ComfyUI"
